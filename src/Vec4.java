26e5c93cb383967e8928d2f604dd1cadcd7dfa62
marcus-elia <mselia@uvm.edu>
2020-04-30 17:13:24 -0400

    Added vec4 class

---
+++ src/Vec4.java
@@ -0,0 +1,107 @@
+public class Vec4
+{
+    public double x,y,z,w;
+
+    public Vec4(double inputX, double inputY, double inputZ, double inputW)
+    {
+        x = inputX;
+        y = inputY;
+        z = inputZ;
+        w = inputW;
+    }
+
+    // =====================================
+    //
+    //            Normalizing
+    //
+    // =====================================
+    public double magnitude()
+    {
+        return Math.sqrt(x*x + y*y + z*z + w*w);
+    }
+
+    public void normalize()
+    {
+        double mag = this.magnitude();
+        if(mag != 0)
+        {
+            this.scalarMultiply(1/mag);
+        }
+    }
+
+    public void divideByW()
+    {
+        if(w != 0)
+        {
+            x = x / w;
+            y = y / w;
+            z = z / w;
+            w = 1;
+        }
+    }
+
+    // =====================================
+    //
+    //          Vector Operations
+    //
+    // =====================================
+
+    // Multiplies the components of this by a
+    public Vec4 scalarMultiply(double a)
+    {
+        x = x*a;
+        y = y*a;
+        z = z*a;
+        w = w*a;
+        return this;
+    }
+
+    // Adds other vector to this
+    public Vec4 add(Vec4 v)
+    {
+        x += v.x;
+        y += v.y;
+        z += v.z;
+        w += v.w;
+        return this;
+    }
+    public Vec4 subtract(Vec4 v)
+    {
+        x -= v.x;
+        y -= v.y;
+        z -= v.z;
+        w -= v.w;
+        return this;
+    }
+
+    // Standard dot product
+    public double dotProduct(Vec4 v)
+    {
+        return x*v.x + y*v.y + z*v.z + w*v.w;
+    }
+
+    // =====================================
+    //
+    //            Static methods
+    //
+    // =====================================
+    public static Vec4 scalarMultiply(double a, Vec4 v)
+    {
+        return v.scalarMultiply(a);
+    }
+
+    public static Vec4 add(Vec4 v1, Vec4 v2)
+    {
+        return v1.add(v2);
+    }
+
+    public static Vec4 subtract(Vec4 v1, Vec4 v2)
+    {
+        return v1.subtract(v2);
+    }
+
+    public static double dotProduct(Vec4 v1, Vec4 v2)
+    {
+        return v1.dotProduct(v2);
+    }
+}
